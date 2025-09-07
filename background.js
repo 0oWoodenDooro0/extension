b6a0c008963c33c1d028ee9df130662cd119342c
WoodenDoor <vincent031525@gmail.com>
2025-09-07 15:29:10 +0800

    v1.2

--- background.js
+++ background.js
@@ -1,72 +1,110 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: "searchOnMis",
-        title: "Search on Mis",
-        contexts: ["selection"]
-    });
-    chrome.contextMenus.create({
-        id: "searchOnSiro",
-        title: "Search on Siro",
-        contexts: ["selection"]
-    });
+function createContextMenu() {
+  browser.contextMenus.create({
+    id: "searchOnMis", title: "Search on Mis", contexts: ["selection"]
+  })
+  browser.contextMenus.create({
+    id: "searchOnSiro", title: "Search on Siro", contexts: ["selection"]
+  })
+  browser.contextMenus.create({
+    id: "searchOnVida", title: "Search on Vida", contexts: ["selection"]
+  })
+  browser.contextMenus.create({
+    id: "searchOnVidc", title: "Search on Vidc", contexts: ["selection"]
+  })
+}
+
+browser.runtime.onStartup.addListener(createContextMenu)
+browser.runtime.onInstalled.addListener(createContextMenu)
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "searchOnMis") {
+    const query = info.selectionText.trim();
+    searchOnMissav(query)
+  } else if (info.menuItemId === "searchOnSiro") {
+    const query = info.selectionText.trim();
+    searchOnSiro(query)
+  } else if (info.menuItemId === "searchOnVida") {
+    const query = info.selectionText.trim()
+    searchOnVida(query)
+  } else if (info.menuItemId === "searchOnVidc") {
+    const query = info.selectionText.trim()
+    searchOnVidc(query)
+  }
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "searchOnMis") {
-        const query = info.selectionText.trim();
-        data = query.match(/([a-zA-z]+)(0{1,})?-?([0-9]{3,})/)
-        console.log(data)
-        // console.log(query);
-        const misSearchUrl = `https://missav.ws/search/${encodeURIComponent(data[1] + "-" + data[3])}`;
-
-        // console.log(misSearchUrl);
-        chrome.tabs.create({url: misSearchUrl});
-    } else if (info.menuItemId === "searchOnSiro") {
-        const query = info.selectionText.trim();
-        // console.log(query);
-        const siroSearchUrl = `https://sirowiki.com/search/?keyword=${encodeURIComponent(query)}`;
-        // console.log(siroSearchUrl);
-        chrome.tabs.create({url: siroSearchUrl});
-    }
-});
-
-chrome.commands.onCommand.addListener((command) => {
-    if (command === "search_on_mis") {
-        console.log(command);
-        chrome.tabs.query({currentWindow: true, active: true}, function (result) {
-            tab = result[0];
-            chrome.scripting.executeScript({
-                target: {tabId: tab.id},
-                function: () => {
-                    return getSelection().toString();
-                }
-            }).then((res) => {
-                const query = res[0].result.trim();
-                data = query.match(/([a-zA-z]+)(0{1,})?-?([0-9]{3,})/)
-                console.log(data)
-                // console.log(query);
-                const misSearchUrl = `https://missav.ws/search/${encodeURIComponent(data[1] + "-" + data[3])}`;
-
-                // console.log(misSearchUrl);
-                chrome.tabs.create({url: misSearchUrl});
-            });
-        });
-    } else if (command === "search_on_siro") {
-        console.log(command);
-        chrome.tabs.query({currentWindow: true, active: true}, function (result) {
-            tab = result[0];
-            chrome.scripting.executeScript({
-                target: {tabId: tab.id},
-                function: () => {
-                    return getSelection().toString();
-                }
-            }).then((res) => {
-                const query = res[0].result.trim();
-                // console.log(query);
-                const siroSearchUrl = `https://sirowiki.com/search/?keyword=${encodeURIComponent(query)}`;
-                // console.log(siroSearchUrl);
-                chrome.tabs.create({url: siroSearchUrl});
-            });
-        });
-    }
+browser.commands.onCommand.addListener((command) => {
+  browser.tabs.query({ currentWindow: true, active: true }, function (result) {
+    let tab = result[0];
+    browser.scripting.executeScript({
+      target: { tabId: tab.id }, function: () => {
+        return window.getSelection().toString();
+      }
+    }, (selection) => {
+      const query = selection[0].result.trim();
+      if (command === "search_on_mis") {
+        searchOnMissav(query)
+      } else if (command === "search_on_siro") {
+        searchOnSiro(query)
+      }
+    });
+  });
 })
+
+function searchOnMissav(query) {
+  const data = query.match(/([a-zA-Z]+)(0+)?-?(\d{3,})/)
+  let id = (data === null) ? query : data[1] + "-" + data[3]
+  const misSearchUrl = `https://missav.ai/search/${encodeURIComponent(id)}`;
+  browser.tabs.create({ url: misSearchUrl });
+}
+
+function searchOnSiro(query) {
+  const siroSearchUrl = `https://sirowiki.com/search/?keyword=${encodeURIComponent(query)}`;
+  browser.tabs.create({ url: siroSearchUrl });
+}
+
+function searchOnVida(query) {
+  const data = query.match(/([a-zA-Z]+)(0+)?-?(\d{3,})/)
+  let id = (data === null) ? query : data[1] + " " + data[3]
+  const vidaSearchUrl = `https://video.dmm.co.jp/av/list/?key=${encodeURIComponent(id)}`;
+  browser.tabs.create({ url: vidaSearchUrl });
+}
+
+function searchOnVidc(query) {
+  const data = query.match(/([a-zA-Z]+)(0+)?-?(\d{3,})/)
+  let id = (data === null) ? query : data[1] + " " + data[3]
+  const vidcSearchUrl = `https://video.dmm.co.jp/amateur/list/?key=${encodeURIComponent(id)}`;
+  browser.tabs.create({ url: vidcSearchUrl });
+}
+
+
+function cleanUrlParameters(details) {
+  const originalUrl = details.url;
+
+  if (details.type !== 'main_frame') {
+    return;
+  }
+
+  const url = new URL(originalUrl);
+  const params = url.searchParams;
+
+  if (params.has('id') && params.size > 1) {
+    const idValue = params.get('id');
+
+    const newUrl = `${url.origin}${url.pathname}?id=${encodeURIComponent(idValue)}`;
+
+    if (originalUrl !== newUrl) {
+      return { redirectUrl: newUrl };
+    }
+  }
+
+  return;
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  cleanUrlParameters,
+  {
+    urls: ["https://video.dmm.co.jp/*"],
+    types: ["main_frame"]
+  },
+  ["blocking"]
+);
---
+++ sidebar/sidebar.js
@@ -0,0 +1,261 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const createCollectionButton = document.getElementById('createCollectionButton');
+  createCollectionButton.onclick = createCollection;
+  const exportCollectionButton = document.getElementById('exportCollectionButton');
+  exportCollectionButton.onclick = exportCollections;
+  const importCollectionButton = document.getElementById('importCollectionButton');
+  importCollectionButton.onclick = importCollections;
+  const fileInput = document.getElementById('fileInput');
+  fileInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        try {
+          browser.storage.local.set({ collections: JSON.parse(e.target.result) }, () => {
+            loadCollections();
+          })
+        } catch (e) {
+          console.error('Error', e);
+        }
+      }
+      reader.readAsText(file);
+    }
+  });
+  const backToCollectionButton = document.getElementById('backToCollectionButton');
+  backToCollectionButton.onclick = backToCollections;
+  const addToCollectionButton = document.getElementById('addToCollectionButton');
+  addToCollectionButton.onclick = addToCollection;
+  const randomButton = document.getElementById('randomButton');
+  randomButton.onclick = random;
+
+  const itemSearchInput = document.getElementById('itemSearchInput');
+  const clearSearchButton = document.getElementById('clearSearchButton');
+
+  const collectionList = document.getElementById('collectionList');
+  const itemList = document.getElementById('itemList');
+  const collectionsDiv = document.getElementById('collections');
+  const itemsDiv = document.getElementById('items');
+
+  let collections = [];
+  let currentCollectionIndex = -1;
+
+  itemSearchInput.addEventListener('input', () => {
+    displayCollectionItems();
+  });
+
+  clearSearchButton.addEventListener('click', () => {
+    itemSearchInput.value = '';
+    displayCollectionItems();
+    itemSearchInput.focus();
+  });
+
+  function loadCollections() {
+    browser.storage.local.get('collections', (data) => {
+      collections = data.collections || [];
+      displayCollections();
+    });
+  }
+
+  function displayCollections() {
+    collectionsDiv.style.display = 'block';
+    itemsDiv.style.display = 'none';
+    collectionList.innerHTML = '';
+    for (let i = 0; i < collections.length; i++) {
+      const listItem = document.createElement('li');
+      const collectionText = document.createElement('span');
+      collectionText.innerText = collections[i].title + `(${collections[i].items.length})`;
+      listItem.className = "collectionItem";
+      listItem.addEventListener('click', () => {
+        currentCollectionIndex = i;
+        itemSearchInput.value = '';
+        displayCollectionItems();
+      })
+      listItem.appendChild(collectionText);
+
+      const collectionSettings = document.createElement('div');
+      collectionSettings.className = "collectionSettings";
+      collectionSettings.innerText = "⚙️";
+
+      const dropdown = document.createElement('div');
+      dropdown.className = "dropdown-content";
+      const renameItem = document.createElement('span');
+      renameItem.innerText = "Rename";
+      renameItem.addEventListener('click', (event) => {
+        event.stopPropagation();
+        showRenameInput(i);
+      });
+      const renameDiv = document.createElement('div');
+      renameDiv.className = "rename-content";
+      const renameInput = document.createElement('input');
+      renameInput.className = "renameInput";
+      renameInput.type = 'text';
+      renameInput.addEventListener('click', (event) => {
+        event.stopPropagation();
+      });
+      renameInput.value = collections[i].title;
+      const renameButton = document.createElement('button');
+      renameButton.innerText = 'Save';
+      renameButton.addEventListener('click', (event) => {
+        event.stopPropagation();
+        renameCollection(i);
+      });
+      renameDiv.appendChild(renameInput);
+      renameDiv.appendChild(renameButton);
+
+      const deleteItem = document.createElement('span');
+      deleteItem.innerText = "Delete";
+      deleteItem.addEventListener('click', (event) => {
+        event.stopPropagation();
+        deleteCollection(i);
+      });
+      listItem.appendChild(renameDiv);
+      dropdown.appendChild(renameItem);
+      dropdown.appendChild(deleteItem);
+      collectionSettings.appendChild(dropdown);
+      listItem.appendChild(collectionSettings);
+      collectionList.appendChild(listItem);
+    }
+  }
+
+  function displayCollectionItems() {
+    collectionsDiv.style.display = 'none';
+    itemsDiv.style.display = 'block';
+    itemList.innerHTML = '';
+
+    const currentItems = collections[currentCollectionIndex].items;
+    const collectionTitle = document.getElementById('itemTitle');
+    collectionTitle.innerText = collections[currentCollectionIndex].title;
+    const searchTerm = itemSearchInput.value.toLowerCase();
+    const searchedItems = currentItems.filter(item => item.title.toLowerCase().includes(searchTerm) || item.url.toLowerCase().includes(searchTerm));
+
+    searchedItems.forEach(item => {
+      const originalIndex = currentItems.indexOf(item);
+
+      const listItem = document.createElement('li');
+      const itemText = document.createElement('span')
+
+      itemText.innerText = item.title
+      listItem.className = "collectionItem"
+      listItem.addEventListener('click', () => {
+        browser.tabs.create({ url: item.url });
+      });
+      listItem.appendChild(itemText)
+
+      const itemSettings = document.createElement('div')
+      itemSettings.className = "collectionSettings"
+      itemSettings.innerText = "⚙️"
+
+      const dropdown = document.createElement('div')
+      dropdown.className = "dropdown-content"
+
+      const deleteItem = document.createElement('span')
+      deleteItem.innerText = "刪除"
+      deleteItem.addEventListener('click', (event) => {
+        event.stopPropagation()
+        removeItem(originalIndex)
+      })
+
+      dropdown.appendChild(deleteItem)
+      itemSettings.appendChild(dropdown)
+      listItem.appendChild(itemSettings)
+      itemList.appendChild(listItem)
+    });
+  }
+
+  function addToCollection() {
+    browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const currentTab = tabs[0];
+      if (currentTab && !checkDuplicateItem(collections[currentCollectionIndex].items, currentTab.url)) {
+        collections[currentCollectionIndex].items.push({ title: currentTab.title, url: currentTab.url });
+        saveCollections();
+        displayCollectionItems();
+      }
+    });
+  }
+
+  function checkDuplicateItem(items, url) {
+    for (let key in items) {
+      if (items[key].url === url) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function backToCollections() {
+    currentCollectionIndex = -1;
+    displayCollections();
+  }
+
+  function showRenameInput(index) {
+    const renameDiv = collectionList.children[index].querySelector('.rename-content');
+    renameDiv.style.display = 'inline-block';
+    collectionList.children[index].children[0].innerText = "";
+  }
+
+  function renameCollection(index) {
+    const renameInput = collectionList.children[index].querySelector('.renameInput');
+    collections[index].title = renameInput.value;
+    saveCollections();
+    displayCollections();
+  }
+
+  function deleteCollection(index) {
+    collections.splice(index, 1);
+    saveCollections();
+    displayCollections();
+  }
+
+  function createCollection() {
+    collections.push({ title: "New Collection", items: [] });
+    saveCollections();
+    displayCollections();
+  }
+
+  function removeItem(index) {
+    const items = collections[currentCollectionIndex].items;
+    items.splice(index, 1);
+    saveCollections();
+    displayCollectionItems();
+  }
+
+  function random() {
+    const items = collections[currentCollectionIndex].items;
+    if (items.length > 0) {
+      const randomItem = items[Math.floor((Math.random() * items.length))];
+      itemSearchInput.value = randomItem.title;
+      displayCollectionItems();
+      browser.tabs.create({ url: randomItem.url });
+    }
+  }
+
+  function exportCollections() {
+    browser.storage.local.get('collections', (data) => {
+      const collections = data.collections || {};
+      const json = JSON.stringify(collections, null, 2);
+      const blob = new Blob([json], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+
+      const filename = `collections-backup-${new Date().toDateString().replaceAll(' ', '_')}.json`;
+      browser.downloads.download({
+        url: url,
+        filename: filename,
+        saveAs: true
+      }).then(() => {
+        URL.revokeObjectURL(url);
+      })
+    })
+  }
+
+  function importCollections() {
+    const fileInput = document.getElementById('fileInput');
+    fileInput.click();
+  }
+
+  function saveCollections() {
+    browser.storage.local.set({ collections: collections });
+  }
+
+  loadCollections();
+});
